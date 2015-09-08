28f18437a9a83a574718c21e5122ae9c87b22d95
varunrao <root@ip-10-6-3-19.ec2.internal>
2015-09-08 18:55:33 -0400

    push

--- solr-validate.sh
+++ solr-validate.sh
@@ -1,2 +1,3 @@
-sudo -u solr cd /opt/lucidworks-hdpsearch/solr/server/solr/configsets/hdp1
-sudo -u solr hadoop jar hadoop-lws-job-1.2.0-0-0.jar com.lucidworks.hadoop.ingest.IngestJob -Dlww.commit.on.close=true -DcsvFieldMapping=0=id,1=text -cls com.lucidworks.hadoop.ingest.CSVIngestMapper -c hdp1 -i /user/solr/data/csv/mydata.csv -of com.lucidworks.hadoop.io.LWMapRedOutputFormat -s http://ip-10-6-3-28.ec2.internal:8983/solr
+su solr
+cd /opt/lucidworks-hdpsearch/solr/server/solr/configsets/hdp1
+hadoop jar hadoop-lws-job-1.2.0-0-0.jar com.lucidworks.hadoop.ingest.IngestJob -Dlww.commit.on.close=true -DcsvFieldMapping=0=id,1=text -cls com.lucidworks.hadoop.ingest.CSVIngestMapper -c hdp1 -i /user/solr/data/csv/mydata.csv -of com.lucidworks.hadoop.io.LWMapRedOutputFormat -s http://ip-10-6-3-28.ec2.internal:8983/solr
